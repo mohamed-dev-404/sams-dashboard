3f7564c5cff5aa6e5f076b71cd2b962a4c7b9011
Yomna Abdelmegeed <yomna.tota4@gmail.com>
2026-02-04 03:01:02 +0200

    feat(core): add app constants with base URL configuration.

--- lib/core/utils/constants/app_constants.dart
+++ lib/core/utils/constants/app_constants.dart
@@ -2,7 +2,7 @@ class AppConstants {
   AppConstants._();
 
   //? Base URL
-  static String baseUrl = 'https://testapi.io/api/';
+  static String baseUrl = 'http://localhost:3000/';
   // static const String kToken = 'token';
   // static const String isLoggedIn = 'isLoggedIn';
   // static const kNavigationTransition = Duration(milliseconds: 1000);
