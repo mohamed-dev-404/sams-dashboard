dd43627d8a79a583fe54070ed6e80277c49eac99
MohamedIbrahemHamad <mohammedhamad.dev@gmail.com>
2026-02-06 23:32:14 +0200

    chore(images): add SVG images and add AppImages class for asset management

--- lib/core/utils/assets/app_images.dart
+++ lib/core/utils/assets/app_images.dart
@@ -1,6 +1,9 @@
 class AppImages {
   AppImages._();
 
-  static const String roleIcon = 'assets/images/icons/Role.svg';
-  static const String statusIcon = 'assets/images/icons/Status.svg';
+  static const String imagesSuccessDialog = 'assets/images/success_dialog.svg';
+  static const String imagesFailureDialog = 'assets/images/failure_dialog.svg';
+  static const String imagesSuccessWebDialogBubblesBg = 'assets/images/success_web_dialog_bubbles_bg.svg';
+  static const String imagesWebDialogSuccess = 'assets/images/web_dialog_success.svg';
+  static const String imagesWebAuthSider = 'assets/images/web_auth_sider.svg';
 }
