bda1e2a23d38982cc4b3ad88a96edd4af55d5fe7
MohamedIbrahemHamad <mohammedhamad.dev@gmail.com>
2026-02-06 23:34:32 +0200

    chore(dialogs): implement AppFailureDialog and AppSuccessDialog for error and success handling

--- lib/core/widgets/app_error_dialog.dart
+++ lib/core/widgets/app_error_dialog.dart
@@ -1,1 +1,59 @@
-//todo custom error dialog widget used in the app
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:sams_dashboard/core/utils/assets/app_images.dart';
+import 'package:sams_dashboard/core/utils/colors/app_colors.dart';
+import 'package:sams_dashboard/core/utils/styles/app_styles.dart';
+
+class AppFailureDialog extends StatelessWidget {
+  final String title;
+  final String message;
+  final VoidCallback onTapped;
+
+  const AppFailureDialog({
+    super.key,
+    required this.title,
+    required this.message,
+    required this.onTapped,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTapped,
+      child: Dialog(
+        backgroundColor: AppColors.whiteLight,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(80),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(24),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              SvgPicture.asset(
+                AppImages.imagesFailureDialog,
+              ),
+              const SizedBox(height: 50),
+              Text(
+                title,
+                style: AppStyles.mobileBodyLargeSb.copyWith(
+                  color: AppColors.primaryDarkHover,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 8),
+              Text(
+                message,
+                style: AppStyles.mobileBodyMediumRg.copyWith(
+                  color: AppColors.primaryDark,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 32),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
