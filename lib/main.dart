1e2e8e10e0fa5143e490f2f63d0eea5d93b3909d
MohamedIbrahemHamad <mohammedhamad.dev@gmail.com>
2026-02-09 00:31:05 +0200

    chore: add flutter_web_plugins as a direct dependency and configure URL strategy

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:sams_dashboard/app_initializer.dart';
 import 'package:sams_dashboard/my_app.dart';
 
 void main() async {
   // Ensure Flutter binding is initialized
   WidgetsFlutterBinding.ensureInitialized();
+  // Removes the '#' from the URL
+  usePathUrlStrategy();
   // Initialize app services
   await AppInitializer.init();
 
