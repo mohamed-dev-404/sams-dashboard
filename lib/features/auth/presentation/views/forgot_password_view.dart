cdb2bce7af698f10b1cd8e29985b5e46da0e5615
MohamedIbrahemHamad <mohammedhamad.dev@gmail.com>
2026-02-13 03:47:05 +0200

    feat(auth view): finish forget password view UI

--- lib/features/auth/presentation/views/forgot_password_view.dart
+++ lib/features/auth/presentation/views/forgot_password_view.dart
@@ -1,4 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:sams_dashboard/core/enums/text_field_type.dart';
+import 'package:sams_dashboard/core/models/app_button_style_model.dart';
+import 'package:sams_dashboard/core/utils/assets/app_icons.dart';
+import 'package:sams_dashboard/core/utils/colors/app_colors.dart';
+import 'package:sams_dashboard/core/utils/router/routes_name.dart';
+import 'package:sams_dashboard/core/utils/styles/app_styles.dart';
+import 'package:sams_dashboard/core/widgets/app_button.dart';
+import 'package:sams_dashboard/core/widgets/app_text_field.dart';
+import 'package:sams_dashboard/core/widgets/svg_icon.dart';
+import 'package:sams_dashboard/core/widgets/titled_input_field.dart';
+import 'package:sams_dashboard/features/auth/presentation/views/widgets/auth_sider_image.dart';
 
 class ForgotPasswordView extends StatefulWidget {
   const ForgotPasswordView({super.key});
@@ -25,15 +37,109 @@ class _ForgotPasswordViewState extends State<ForgotPasswordView> {
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Form(
-        key: _formKey,
-        child: const SingleChildScrollView(
-          child: Column(
-            children: [],
+      body: Row(
+        children: [
+          width <= 830
+              ? const SizedBox(
+                  height: double.infinity,
+                )
+              : const AuthSiderImage(),
+          const SizedBox(
+            width: 42,
           ),
-        ),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Form(
+                key: _formKey,
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                child: Padding(
+                  padding: const EdgeInsets.only(right: 32.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      _buildHeaderSection(),
+
+                      const SizedBox(height: 32),
+
+                      _buildEmailField(),
+
+                      const SizedBox(height: 90),
+                      
+                      AppButton(
+                        model: AppButtonStyleModel(
+                          label: 'Next Step',
+                          onPressed: () {
+                            _submitForm();
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
+  }
+
+  Widget _buildHeaderSection() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Forgot Password',
+          style: AppStyles.webTitleLargeMd.copyWith(
+            color: AppColors.primaryDarkHover,
+          ),
+        ),
+        const SizedBox(
+          height: 32,
+        ),
+        Text(
+          'reset password?',
+          style: AppStyles.web32Semibold.copyWith(
+            color: AppColors.primaryDarkHover,
+          ),
+        ),
+        const SizedBox(
+          height: 16,
+        ),
+        Text(
+          'Please enter your academic email\nto reset your password ',
+          style: AppStyles.web24Regular.copyWith(
+            color: AppColors.primaryDarkHover,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildEmailField() {
+    return TitledInputField(
+      label: 'Academic Email',
+      child: AppTextField(
+        controller: _emailController,
+        prefixIcon: const SvgIcon(
+          AppIcons.iconsProfileEmail,
+        ),
+        hintText: 'example@example.com',
+        textFieldType: TextFieldType.email,
+        onFieldSubmitted: (_) {
+          _submitForm();
+        },
+      ),
+    );
+  }
+
+  void _submitForm() {
+    final bool isValid = _formKey.currentState!.validate();
+    if (isValid) {
+      //todo call cubit sendResetCode method here
+      context.go(RoutesName.verifyOtp); //!temp navigation
+    }
   }
 }
