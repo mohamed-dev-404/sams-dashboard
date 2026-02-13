67eaf0bff2bcdfe1ed1178a10463e0514e4b4ab2
MohamedIbrahemHamad <mohammedhamad.dev@gmail.com>
2026-02-13 03:53:54 +0200

    feat(auth view): finish reset password view UI

--- lib/features/auth/presentation/views/reset_password_view.dart
+++ lib/features/auth/presentation/views/reset_password_view.dart
@@ -1,4 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:sams_dashboard/core/enums/password_field_type.dart';
+import 'package:sams_dashboard/core/models/app_button_style_model.dart';
+import 'package:sams_dashboard/core/utils/colors/app_colors.dart';
+import 'package:sams_dashboard/core/utils/router/routes_name.dart';
+import 'package:sams_dashboard/core/utils/styles/app_styles.dart';
+import 'package:sams_dashboard/core/widgets/app_button.dart';
+import 'package:sams_dashboard/core/widgets/password_text_field.dart';
+import 'package:sams_dashboard/core/widgets/titled_input_field.dart';
+import 'package:sams_dashboard/features/auth/presentation/views/widgets/auth_sider_image.dart';
 
 class ResetPasswordView extends StatefulWidget {
   const ResetPasswordView({super.key});
@@ -10,6 +20,7 @@ class ResetPasswordView extends StatefulWidget {
 class _ResetPasswordViewState extends State<ResetPasswordView> {
   late final TextEditingController _newPasswordController;
   late final TextEditingController _confirmPasswordController;
+  late final FocusNode _confirmpassFocusNode;
   final _formKey = GlobalKey<FormState>();
 
   @override
@@ -17,26 +28,114 @@ class _ResetPasswordViewState extends State<ResetPasswordView> {
     super.initState();
     _newPasswordController = TextEditingController();
     _confirmPasswordController = TextEditingController();
+    _confirmpassFocusNode = FocusNode();
   }
 
   @override
   void dispose() {
     _newPasswordController.dispose();
     _confirmPasswordController.dispose();
+    _confirmpassFocusNode.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Form(
-        key: _formKey,
-        child: const SingleChildScrollView(
-          child: Column(
-            children: [],
+      body: Row(
+        children: [
+          width <= 830
+              ? const SizedBox(
+                  height: double.infinity,
+                )
+              : const AuthSiderImage(),
+          const SizedBox(
+            width: 42,
           ),
-        ),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Form(
+                key: _formKey,
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                child: Padding(
+                  padding: const EdgeInsets.only(right: 32.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      _buildHeaderText(),
+
+                      const SizedBox(height: 32),
+
+                      _buildResetPassFields(),
+
+                      const SizedBox(height: 90),
+
+                      AppButton(
+                        model: AppButtonStyleModel(
+                          label: 'Change Password',
+                          onPressed: () {
+                            _submitForm();
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
+  }
+
+  Widget _buildHeaderText() {
+    return Text(
+      'Reset Password',
+      style: AppStyles.webTitleLargeMd.copyWith(
+        color: AppColors.primaryDarkHover,
+      ),
+    );
+  }
+
+  Widget _buildResetPassFields() {
+    return Column(
+      spacing: 16,
+      children: [
+        TitledInputField(
+          label: 'New Password',
+          child: AppPasswordField(
+            controller: _newPasswordController,
+            hintText: 'enter new password',
+            passwordFieldType: PasswordFieldType.originalPassword,
+            onFieldSubmitted: (_) => FocusScope.of(
+              context,
+            ).requestFocus(_confirmpassFocusNode),
+          ),
+        ),
+        TitledInputField(
+          label: 'Confirm new Password',
+          child: AppPasswordField(
+            controller: _confirmPasswordController,
+            originalController: _newPasswordController,
+            focusNode: _confirmpassFocusNode,
+            hintText: 'confirm your new password',
+            passwordFieldType: PasswordFieldType.confirmPassword,
+            onFieldSubmitted: (_) {
+              _submitForm();
+            },
+          ),
+        ),
+      ],
+    );
+  }
+
+  void _submitForm() {
+    final bool isValid = _formKey.currentState!.validate();
+    if (isValid) {
+      //todo call cubit sendResetCode method here
+      context.go(RoutesName.home); //!temp navigation
+    }
   }
 }
