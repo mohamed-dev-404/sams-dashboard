c2a91f655e5bb83256b714404d863cb739473aea
MohamedIbrahemHamad <mohammedhamad.dev@gmail.com>
2026-02-13 07:33:02 +0200

    feat(auth data): implement Login request and cache required data

--- lib/features/auth/data/repos/auth_repo.dart
+++ lib/features/auth/data/repos/auth_repo.dart
@@ -1,17 +1,27 @@
 //todo auth repos
 import 'package:dartz/dartz.dart';
-import 'package:sams_dashboard/features/auth/data/models/login_response_model.dart';
+import 'package:sams_dashboard/features/auth/data/models/login_model/login_model.dart';
 
 abstract class AuthRepo {
-  // Returns Either<Failure, LoginModel> using dartz or similar
-  Future<Either<String, LoginResponseModel>> login({required String email, required String password});
-  
-  // Forgot Password (usually returns a success message or void)
-  Future<Either<String, String>> forgotPassword({required String email});
-  
+  //? login
+  Future<Either<String, LoginModel>> login({
+    required String email,
+    required String password,
+  });
+
+  //? Forgot Password (usually returns a failure message or void)
+  Future<Either<String, void>> forgotPassword({required String email});
+
   // Verify OTP
-  Future<Either<String, String>> verifyOtp({required String email, required String otp});
-  
+  Future<Either<String, String>> verifyOtp({
+    required String email,
+    required String otp,
+  });
+
   // Reset Password
-  Future<Either<String, String>> resetPassword({required String email, required String otp, required String newPassword});
+  Future<Either<String, String>> resetPassword({
+    required String email,
+    required String otp,
+    required String newPassword,
+  });
 }
--- lib/features/auth/data/repos/auth_repo_impl.dart
+++ lib/features/auth/data/repos/auth_repo_impl.dart
@@ -1,6 +1,12 @@
 import 'package:dartz/dartz.dart';
+import 'package:sams_dashboard/core/cache/get_storage.dart';
+import 'package:sams_dashboard/core/cache/secure_storage.dart';
+import 'package:sams_dashboard/core/errors/exceptions/api_exception.dart';
 import 'package:sams_dashboard/core/network/api_consumer.dart';
-import 'package:sams_dashboard/features/auth/data/models/login_response_model.dart';
+import 'package:sams_dashboard/core/utils/constants/api_endpoints.dart';
+import 'package:sams_dashboard/core/utils/constants/api_keys.dart';
+import 'package:sams_dashboard/core/utils/constants/cache_keys.dart';
+import 'package:sams_dashboard/features/auth/data/models/login_model/login_model.dart';
 import 'package:sams_dashboard/features/auth/data/repos/auth_repo.dart';
 
 class AuthRepoImpl implements AuthRepo {
@@ -8,18 +14,46 @@ class AuthRepoImpl implements AuthRepo {
 
   AuthRepoImpl(this.api);
 
+  //! login request
   @override
-  Future<Either<String, String>> forgotPassword({required String email}) {
-    // TODO: implement forgotPassword
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<Either<String, LoginResponseModel>> login({
+  Future<Either<String, LoginModel>> login({
     required String email,
     required String password,
-  }) {
-    // TODO: implement login
+  }) async {
+    try {
+
+      // hit login request
+      Map<String, dynamic> response = await api.post(
+        EndPoints.login,
+        data: {
+          ApiKeys.academicEmail: email,
+          ApiKeys.password: password,
+        },
+      );
+
+      //parsing and initialize loginModel
+      final dataJson = response[ApiKeys.data];
+      LoginModel loginModel = LoginModel.fromJson(dataJson);
+
+      //cache tokens and required data to display in home
+      await _cacheUserData(loginModel);
+
+      return Right(loginModel); // success case, return loginModel
+
+    } on ApiException catch (e) {
+      return Left(e.errorModel.errorMessage); // failure case
+    } catch (e) {
+      return Left(e.toString()); // failure case
+    }
+  }
+
+
+  //! forget password request
+  @override
+  Future<Either<String, String>> forgotPassword({required String email}) {
+
+    
+
     throw UnimplementedError();
   }
 
@@ -29,7 +63,6 @@ class AuthRepoImpl implements AuthRepo {
     required String otp,
     required String newPassword,
   }) {
-    // TODO: implement resetPassword
     throw UnimplementedError();
   }
 
@@ -38,7 +71,29 @@ class AuthRepoImpl implements AuthRepo {
     required String email,
     required String otp,
   }) {
-    // TODO: implement verifyOtp
     throw UnimplementedError();
+  }
+
+
+//* helper methods
+  Future<void> _cacheUserData(LoginModel loginModel) async {
+    await SecureStorageService.instance.saveAccessToken(
+      loginModel.accessToken ?? '',
+    );
+    await SecureStorageService.instance.saveRefreshToken(
+      loginModel.refreshToken ?? '',
+    );
+    await GetStorageHelper.write(
+      CacheKeys.name,
+      loginModel.user?.name ?? '',
+    );
+    await GetStorageHelper.write(
+      CacheKeys.academicEmail,
+      loginModel.user?.academicEmail ?? '',
+    );
+    await GetStorageHelper.write(
+      CacheKeys.profilePic,
+      loginModel.user?.profilePic ?? '',
+    );
   }
 }
